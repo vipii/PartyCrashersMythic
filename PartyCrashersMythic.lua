762c28231d1b451d4c598e12b53a33ec0849bbbf
Christophe Vanleeuw <christophe.vanleeuw@expliciet.be>
2023-12-30 14:52:52 +0100

    English localization

--- PartyCrashersMythic.lua
+++ PartyCrashersMythic.lua
@@ -1,22 +1,24 @@
 local PartyCrashersMythicPlus = {}
 local pc = PartyCrashersMythicPlus
 
+-- Command aliases
 SLASH_PARTYCRASHERS1 = "/pc"
 SLASH_PARTYCRASHERS2 = "/partycrashers"
 SlashCmdList["PARTYCRASHERS"] = function(msg)
     pc.handleCommand(msg)
 end
 
+-- Handle command input
 function pc.handleCommand(args)
     local keyLevel, affix = pc.parseArguments(args)
 
     if not pc.isMythicSeasonActive() then
-        print("PartyCrashers Mythic Plus Helper: Er is momenteel geen M+ season actief!")
+        print("PartyCrashers Mythic Plus Helper: No M+ season is currently active!")
         return
     end
 
     if keyLevel and (keyLevel < 2 or keyLevel > 50) then
-        print("PartyCrashers Mythic Plus Helper: Je kan enkel keys tussen 2 en 50 ingeven")
+        print("PartyCrashers Mythic Plus Helper: Key level must be between 2 and 50")
         return
     end
 
@@ -27,25 +29,26 @@ function pc.handleCommand(args)
     end
 end
 
+-- Parse arguments from the command
 function pc.parseArguments(args)
     local keyLevel = tonumber(args:match("^%-?%d+"))
     local affix = args:match("[F|T]$")
     return keyLevel, affix
 end
 
+-- Check if Mythic+ season is active
 function pc.isMythicSeasonActive()
     local currentAffixes = C_MythicPlus.GetCurrentAffixes()
     return currentAffixes and currentAffixes[1]
 end
 
+-- Print usage instructions
 function pc.printUsageInstructions()
-    print("PartyCrashers Mythic Plus Helper kun je gebruiken door de volgende command te gebruiken:")
-    print("/pc <key level>")
-    print("Wil je een speciale week checken? Gebruik dan:")
-    print("/pc <key level> <F|T>")
-    print("Bij problemen? Contacteer Huskii!")
+    print("Usage: /pc <key level>")
+    print("To check a specific week: /pc <key level> <F|T>")
 end
 
+-- Calculate rating based on key level and affix
 function pc.calculateRating(keyLevel, affix)
     local affixMap = { ["T"] = 1, ["F"] = 2 }
     local currentWeek = affix and affixMap[affix] or pc.getCurrentWeekAffixId()
@@ -53,10 +56,12 @@ function pc.calculateRating(keyLevel, affix)
     pc.displayRatingInfo(keyLevel, rating, currentWeek)
 end
 
+-- Get the current week's affix ID
 function pc.getCurrentWeekAffixId()
     return C_MythicPlus.GetCurrentAffixes()[1].id - 8
 end
 
+-- Return rating based on key level
 function pc.getRatingForLevel(keyLevel)
     if keyLevel < 10 then
         return pc.sub10Ratings[keyLevel]
@@ -64,10 +69,12 @@ function pc.getRatingForLevel(keyLevel)
     return keyLevel * 5 + 50 + (keyLevel - 10) * 2
 end
 
+-- Display rating information
 function pc.displayRatingInfo(keyLevel, rating, currentWeek)
-    print("------------------------------------")
-    print("Punten voor +"..keyLevel.." "..pc.affixTypes[currentWeek]..":")
+    print("---- PartyCrashers Mythic Plus Helper ----")
+    print("Points for +"..keyLevel.." "..pc.affixTypes[currentWeek]..":")
 
+    -- Calculate and display points for each map
     for _, mapId in ipairs(C_ChallengeMode.GetMapTable()) do
         local gain = pc.calculateMapScore(mapId, rating)
         if gain > 0 then
@@ -76,40 +83,28 @@ function pc.displayRatingInfo(keyLevel, rating, currentWeek)
         end
     end
 
-    print("Totaal punten: "..pc.totalGain)
-    print("------------------------------------\n")
+    print("Total points: "..pc.totalGain)
+    print("---- PartyCrashers Mythic Plus Helper ----\n")
 end
 
+-- Rating for levels below 10
 pc.sub10Ratings = {
     [2] = 40, [3] = 45, [4] = 55, [5] = 60,
     [6] = 65, [7] = 75, [8] = 80, [9] = 85
 }
 
+-- Affix types
 pc.affixTypes = { "Tyrannical", "Fortified" }
 
+-- Calculate score for a map
 function pc.calculateMapScore(mapId, newScore)
     local affixScores = C_MythicPlus.GetSeasonBestAffixScoreInfoForMap(mapId)
     return pc.calculateScoreGain(affixScores, newScore)
 end
 
+-- Calculate the score gain
 function pc.calculateScoreGain(affixScores, newScore)
     local scores = {0, 0}
 
     if affixScores then
-        for i = 1, 2 do
-            scores[i] = affixScores[i] and affixScores[i].score or 0
-        end
-    end
-
-    local currentBest = math.max(scores[1], scores[2]) * 1.5 + math.min(scores[1], scores[2]) * 0.5
-    local updatedBest = math.max(scores[1], scores[2], newScore) * 1.5 + math.min(scores[1], scores[2], newScore) * 0.5
-
-    local gain = updatedBest - currentBest
-    if gain > 0 then
-        pc.totalGain = pc.totalGain + gain
-    end
-
-    return gain
-end
-
-pc.totalGain = 0
+        for i
